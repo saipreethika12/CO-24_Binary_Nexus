f751567a4abdd7e4fb287435522c0354e1b656c7
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-03-24 20:02:06 +0530

    Updated files

--- Phase 2/Processor.h
+++ Phase 2/Processor.h
@@ -31,23 +31,23 @@ public:
 
         instructionsFile.close();
 
-        std::ifstream instructionsFilee("text1.txt");
-        if (!instructionsFilee.is_open()) {
-            std::cerr << "Error opening file 'text1.txt'." << std::endl;
-            return;
-        }
+        // std::ifstream instructionsFilee("text1.txt");
+        // if (!instructionsFilee.is_open()) {
+        //     std::cerr << "Error opening file 'text1.txt'." << std::endl;
+        //     return;
+        // }
 
-        PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
-        pwof2.readInstructionsFromFile("selection.txt", RAM, visited);
+        // PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
+        // pwof2.readInstructionsFromFile("selection.txt", RAM, visited);
 
-        if (x == 1) {
-            pwof2.Step_count(RAM);
-        } else {
-            PIPE_WF pwf;
-            pwf.Step_countWF(RAM);
-        }
+        // if (x == 1) {
+        //     pwof2.Step_count(RAM);
+        // } else {
+        //     PIPE_WF pwf;
+        //     pwf.Step_countWF(RAM);
+        // }
 
-        instructionsFilee.close();
+        // instructionsFilee.close();
     }
 };
 
