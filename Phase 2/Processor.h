917adc35ef03e0189014bf7a9e3d87210382daae
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-03-27 19:41:01 +0530

    corrected Processor.h

--- Phase 2/Processor.h
+++ Phase 2/Processor.h
@@ -13,41 +13,43 @@ public:
     bool visited[4096] = {0};
 
     void run(int x) {
-        std::ifstream instructionsFile("text.txt");
+        std::ifstream instructionsFile("bubble_sort.txt");
         if (!instructionsFile.is_open()) {
-            std::cerr << "Error opening file 'text.txt'." << std::endl;
+            std::cerr << "Error opening file 'bubble_sort.txt'." << std::endl;
             return;
         }
 
         PIPE_WOF pwof; // Assuming PIPE_WOF extends Core
-        pwof.readInstructionsFromFile("text.txt", RAM, visited);
+        pwof.readInstructionsFromFile("bubble_sort.txt", RAM, visited);
 
         if (x == 1) {
             pwof.Step_count(RAM);
         } else {
             PIPE_WF pwf;
+             pwf.readInstructionsFromFile("bubble_sort.txt",RAM,visited);
             pwf.Step_countWF(RAM);
         }
 
         instructionsFile.close();
 
-        // std::ifstream instructionsFilee("text1.txt");
-        // if (!instructionsFilee.is_open()) {
-        //     std::cerr << "Error opening file 'text1.txt'." << std::endl;
-        //     return;
-        // }
+        std::ifstream instructionsFile("selection_sort.txt");
+         if (!instructionsFilee.is_open()) {
+            std::cerr << "Error opening file 'selection_sort.txt'." << std::endl;
+             return;
+         }
 
-        // PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
-        // pwof2.readInstructionsFromFile("selection.txt", RAM, visited);
+         PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
+         pwof2.readInstructionsFromFile("selection_sort.txt", RAM, visited);
 
-        // if (x == 1) {
-        //     pwof2.Step_count(RAM);
-        // } else {
-        //     PIPE_WF pwf;
-        //     pwf.Step_countWF(RAM);
-        // }
+         if (x == 1) {
+            pwof2.Step_count(RAM);
+         } else {
+             PIPE_WF pwf2;
+          pwf2.readInstructionsFromFile("selection_sort.txt",RAM,visited);
+             pwf2.Step_countWF(RAM);
+         }
 
-        // instructionsFilee.close();
+         instructionsFilee.close();
     }
 };
 
