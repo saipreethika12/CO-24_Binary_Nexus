90f5985542349f2a6efafcae1db87460f5e6575a
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-03-28 09:30:55 +0530

    set latencies

--- Phase 2/main.cpp
+++ Phase 2/main.cpp
@@ -4,23 +4,22 @@ int main()
 {
     Processor processor;
     int choice;
-    std::map <std::string,int> latency_map;
     std::cout << "Enter 1 for pipeline with forwarding and 2 for pipeline without forwarding" << std::endl;
-    int x;
+    int addi,mul,add,sub;
     std::cout << "Enter the latency for ADD:\n"
               << std::endl;
-    std::cin >> x;
-    latency_map["ADD"] = x;
+    std::cin >> add;
     std::cout << "Enter the latency for SUB:\n"
               << std::endl;
-    std::cin >> x;
-    latency_map["SUB"] = x;
+    std::cin >> sub;
     std::cout << "Enter the latency for ADDI:\n"
               << std::endl;
-    std::cin >> x;
-    latency_map["ADDI"] = x;
-    processor.run(1);
-
+    std::cin >> addi;
+    std::cout << "Enter the latency for MUL:\n"
+              << std::endl;
+    std::cin >> mul;
+    processor.set_latencies(addi,add,mul,sub);
+    processor.run(0);
 
     return 0;
 }
