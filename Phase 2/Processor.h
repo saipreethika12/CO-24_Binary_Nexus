1364af14a4c7bcb8187c3154593adf805163f2f2
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-03-28 09:30:13 +0530

    set latencies

--- Phase 2/Processor.h
+++ Phase 2/Processor.h
@@ -6,50 +6,63 @@
 #include "PIPE_WOF.h"
 #include "PIPE_WF.h"
 
-class Processor {
+class Processor
+{
 public:
     char RAM[4096];
     int clock = 0;
     bool visited[4096] = {0};
-
-    void run(int x) {
-        std::ifstream instructionsFile("bubble_sort.txt");
-        if (!instructionsFile.is_open()) {
-            std::cerr << "Error opening file 'bubble_sort.txt'." << std::endl;
+    PIPE_WOF pwof;
+    PIPE_WF pwf;
+    void set_latencies(int addi, int add, int mul, int sub)
+    {
+        pwf.latency_map["ADDI"] = addi;
+        pwf.latency_map["ADD"] = add;
+        pwf.latency_map["MUL"] = mul;
+        pwf.latency_map["SUB"] = sub;
+    }
+    void run(int x)
+    {
+        std::ifstream instructionsFile("text.txt");
+        if (!instructionsFile.is_open())
+        {
+            std::cerr << "Error opening file 'text.txt'." << std::endl;
             return;
         }
 
-        PIPE_WOF pwof; // Assuming PIPE_WOF extends Core
-        pwof.readInstructionsFromFile("bubble_sort.txt", RAM, visited);
+        // Assuming PIPE_WOF extends Core
+        pwof.readInstructionsFromFile("text.txt", RAM, visited);
 
-        if (x == 1) {
+        if (x == 1)
+        {
             pwof.Step_count(RAM);
-        } else {
-            PIPE_WF pwf;
-             pwf.readInstructionsFromFile("bubble_sort.txt",RAM,visited);
+        }
+        else
+        {
+
+            pwf.readInstructionsFromFile("text.txt", RAM, visited);
             pwf.Step_countWF(RAM);
         }
 
         instructionsFile.close();
 
-        std::ifstream instructionsFile("selection_sort.txt");
-         if (!instructionsFilee.is_open()) {
-            std::cerr << "Error opening file 'selection_sort.txt'." << std::endl;
-             return;
-         }
+        // std::ifstream instructionsFilee("text1.txt");
+        // if (!instructionsFilee.is_open()) {
+        //     std::cerr << "Error opening file 'text1.txt'." << std::endl;
+        //     return;
+        // }
 
-         PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
-         pwof2.readInstructionsFromFile("selection_sort.txt", RAM, visited);
+        // PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
+        // pwof2.readInstructionsFromFile("selection.txt", RAM, visited);
 
-         if (x == 1) {
-            pwof2.Step_count(RAM);
-         } else {
-             PIPE_WF pwf2;
-          pwf2.readInstructionsFromFile("selection_sort.txt",RAM,visited);
-             pwf2.Step_countWF(RAM);
-         }
+        // if (x == 1) {
+        //     pwof2.Step_count(RAM);
+        // } else {
+        //     PIPE_WF pwf;
+        //     pwf.Step_countWF(RAM);
+        // }
 
-         instructionsFilee.close();
+        // instructionsFilee.close();
     }
 };
 
