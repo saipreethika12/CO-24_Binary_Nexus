af3b3bc7abe23f1f02121f858083d571fec79773
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-04-03 23:25:20 +0530

    modified processor.h for cache simulation

--- Phase 2/Processor.h
+++ Phase 2/Processor.h
@@ -6,6 +6,7 @@
 #include <cmath>
 #include "PIPE_WOF.h"
 #include "PIPE_WF.h"
+#include "cache_simulator.h"
 
 class Processor
 {
@@ -14,31 +15,13 @@ public:
     int sets;
     int blocks;
     int offset;
-    char ***cache; // cache is a 3D array of pointers to lines in the RAM
     int clock = 0;
     bool visited[4096] = {0};
+    Cache_simulator cacheSimulator;
     PIPE_WOF pwof;
     PIPE_WF pwf;
-    void init_cache(int s, int b, int o)
-    {
-        sets = s;
-        blocks = b;
-        offset = o;
-        cache = new char **[sets];
-        for (int i = 0; i < sets; i++)
-        {
-            cache[i] = new char *[blocks];
-            for (int j = 0; j < blocks; j++)
-            {
-                cache[i][j] = new char[offset];
 
-                for (int k = 0; k < offset; k++)
-                {
-                    cache[i][j][k] = '\0';
-                }
-            }
-        }
-    }
+
     void set_latencies(int addi_lat, int add_lat, int mul_lat, int sub_lat)
     {
         pwf.latency_map["ADDI"] = addi_lat;
@@ -70,45 +53,38 @@ public:
 
         return parameters;
     }
-    std::map<std::string, std::map<std::string, std::string>> parseInputFile(const std::string &filename)
-    {
-        std::map<std::string, std::map<std::string, std::string>> config;
-
-        std::ifstream inputFile(filename);
-        if (!inputFile.is_open())
-        {
-            std::cerr << "Error opening input file." << std::endl;
-            return config;
-        }
-
-        std::string line;
-        std::string section;
-        while (std::getline(inputFile, line))
-        {
-            if (!line.empty() && line.front() == '#')
-            {
-                section = line.substr(1);
-                if (section == "Cache_Configuration" || section == "Memory_access")
-                {
-                    config[section] = getInputParameters(inputFile);
-                }
-            }
-        }
-
-        inputFile.close();
+   std::map<std::string, std::string> parseInputFile(const std::string& filename) {
+    std::map<std::string, std::string>config;
+    std::ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file." << std::endl;
         return config;
     }
-    void set_values(std::map<std::string, std::string> cache_section){
-        int block_size=stoi(cache_section["Block_size"]);
-        int cache_size=stoi(cache_section["Cache_size"]);
-        int associativity=stoi(cache_section["Associativity"]);
-        int n= log10(block_size) / log10(2);
-        this->offset= n;
-        int N= (cache_size*(int)pow(2,10))/block_size;
-        this->blocks=N;
-        this->sets=N/associativity;
 
+    std::string line;
+    std::string section;
+    while (std::getline(inputFile, line)) {
+        if (!line.empty() && line.front() == '#') {
+            section = line.substr(1);
+            if (section == "Cache_Configuration") {
+                config = getInputParameters(inputFile);
+            }
+        }
+    }
 
+    inputFile.close();
+    return config;
+}
+    void set_cache(std::string filename){
+         std::map<std::string, std::string> config = parseInputFile(filename);
+         // Set Cache Configuration
+         unsigned int cache_size = std::stoi(config["Cache_size"]);
+         unsigned  block_size= std::stoi(config["Block_size"]);
+         unsigned  associativity = std::stoi(config["Associativity"]);
+
+         Cache_simulator cache(cache_size,block_size,associativity);
+         this->cacheSimulator= cache;
+       
     }
     void run(int x)
     {
@@ -119,8 +95,8 @@ public:
             return;
         }
         std::string inputFilename = "input.txt";
-        std::map<std::string, std::map<std::string, std::string>> config = parseInputFile(inputFilename);
-        set_values(config["Cache_Configuration"]);
+        std::map<std::string, std::string> config = parseInputFile(inputFilename);
+        //set_values(config["Cache_Configuration"]);
         pwof.readInstructionsFromFile("input.txt", RAM, visited);
 
         if (x == 1)
@@ -135,24 +111,6 @@ public:
         }
 
         instructionsFile.close();
-
-        // std::ifstream instructionsFilee("text1.txt");
-        // if (!instructionsFilee.is_open()) {
-        //     std::cerr << "Error opening file 'text1.txt'." << std::endl;
-        //     return;
-        // }
-
-        // PIPE_WOF pwof2; // Assuming PIPE_WOF extends Core
-        // pwof2.readInstructionsFromFile("selection.txt", RAM, visited);
-
-        // if (x == 1) {
-        //     pwof2.Step_count(RAM);
-        // } else {
-        //     PIPE_WF pwf;
-        //     pwf.Step_countWF(RAM);
-        // }
-
-        // instructionsFilee.close();
     }
 };
 
