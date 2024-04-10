884a3c7915965653e6df0517e7478c184c6104d2
saipreethika12 <128885730+saipreethika12@users.noreply.github.com>
2024-04-10 21:44:15 +0530

    updated main

--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 #include "Processor.h"
 int main()
 {
-      std::cout<<"ls"<<std::endl;
     Processor processor;
-  
     int choice;
-    // std::cout << "Enter 1 for pipeline with forwarding and 2 for pipeline without forwarding" << std::endl;
-    // int addi,mul,add,sub;
-    // std::cout << "Enter the latency for ADD:\n"
-    //           << std::endl;
-    // std::cin >> add;
-    // std::cout << "Enter the latency for SUB:\n"
-    //           << std::endl;
-    // std::cin >> sub;
-    // std::cout << "Enter the latency for ADDI:\n"
-    //           << std::endl;
-    // std::cin >> addi;
-    // std::cout << "Enter the latency for MUL:\n"
-    //           << std::endl;
-    // std::cin >> mul;
-    processor.set_latencies(3,1,1,1);
-    
-    processor.run(1);
+    std::cout << "Enter 1 for pipeline with forwarding and 2 for pipeline without forwarding" << std::endl;
+    int pipeline;
+    std::cin>>pipeline;
+    std::cout<<"Policy 1:viaLRU"<<std::endl;
+    std::cout<<"Policy 2:viaRandom"<<std::endl;
+
+    int addi,mul,add,sub;
+    std::cout << "Enter the latency for ADD:\n"
+              << std::endl;
+    std::cin >> add;
+    std::cout << "Enter the latency for SUB:\n"
+              << std::endl;
+    std::cin >> sub;
+    std::cout << "Enter the latency for ADDI:\n"
+              << std::endl;
+    std::cin >> addi;
+    std::cout << "Enter the latency for MUL:\n"
+              << std::endl;
+    std::cin >> mul;
+
+    processor.set_latencies(addi,add,mul,sub);
+    processor.run(pipeline);
 
     return 0;
 }
